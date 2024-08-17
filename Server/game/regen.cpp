e757d8868f84807b04f508461e89ac7f21235362
iSenji <k.balazs19980925@gmail.com>
2024-08-17 23:15:36 +0200

    v0.2

--- Server/game/regen.cpp
+++ Server/game/regen.cpp
@@ -1,31 +1,86 @@
-//Keress rá (EVENTFUNC(regen_event)) belül:
-	regen_spawn(regen, false);
+//Keress rá:
+	MODE_Z_SECTION,
+	MODE_DIRECTION,
+	MODE_REGEN_TIME,
 
-	return PASSES_PER_SEC(regen->time);
-
-//Add hozzá alötte:
+//Add hozzá utána:
 #ifdef ENABLE_RENEVAL_REGEN
-	if(regen->nextRespawn)
-	{
-		if(regen->nextRespawn <= time(0))
-			regen_spawn(regen, false);
-	}
-	else
-		regen_spawn(regen, false);
-
-	return PASSES_PER_SEC(1);
-#else
-	regen_spawn(regen, false);
-
-	return PASSES_PER_SEC(regen->time);
+	MODE_REGEN_TIME_RANGE,
 #endif
 
 //Keress rá:
-regen->event = event_create(regen_event, info, PASSES_PER_SEC(number(0, 16)) + PASSES_PER_SEC(regen->time));
+	int mode = MODE_TYPE;
+	int tmpTime;
 
-//Cseréld le:
+Add hozzá utána:
 #ifdef ENABLE_RENEVAL_REGEN
-				regen->event = event_create(regen_event, info, PASSES_PER_SEC(1)); 
-#else
-				regen->event = event_create(regen_event, info, PASSES_PER_SEC(number(0, 16)) + PASSES_PER_SEC(regen->time)); 
+	int tmpTimeRange;
+#endif
+
+//Keress rá:
+			case MODE_REGEN_TIME:
+			[...]
+			break;
+
+Add hozzá utána:
+#ifdef ENABLE_RENEVAL_REGEN
+			case MODE_REGEN_TIME_RANGE:
+				regen->timeRange = 0;
+				tmpTimeRange = 0;
+
+				for (i = 0; i < strlen(szTmp); ++i)
+				{
+					switch (szTmp[i])
+					{
+						case 'h':
+							regen->timeRange += tmpTimeRange * 3600;
+							tmpTimeRange = 0;
+							break;
+
+						case 'm':
+							regen->timeRange += tmpTimeRange * 60;
+							tmpTimeRange = 0;
+							break;
+
+						case 's':
+							regen->timeRange += tmpTimeRange;
+							tmpTimeRange = 0;
+							break;
+
+						default:
+							if (szTmp[i] >= '0' && szTmp[i] <= '9')
+							{
+								tmpTimeRange *= 10;
+								tmpTimeRange += (szTmp[i] - '0');
+							}
+					}
+				}
+
+				++mode;
+				break;
+#endif
+
+
+
+//Keress rá (EVENTFUNC(regen_event)) belül:
+	regen_spawn(regen, false);
+	return PASSES_PER_SEC(regen->time);
+
+//Add hozzá előtte:
+#ifdef ENABLE_RENEVAL_REGEN
+	if(regen->nextRespawn > 0)
+	{
+		if(regen->nextRespawn <= time(0))
+		{
+			regen_spawn(regen, false);
+			regen->nextRespawn = 0;
+			return PASSES_PER_SEC(number(regen->time, regen->timeRange));
+		}
+		else
+		{
+			//fix
+			DWORD nextSpawn = static_cast<DWORD>(regen->nextRespawn - time(0));
+			return PASSES_PER_SEC(nextSpawn);
+		}
+	}
 #endif
